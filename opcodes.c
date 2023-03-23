fe6cd1da5072fde12e3450b59a60a8b3101a176f
Nataly-Aketch <nataliebeckie@gmail.com>
2023-03-23 23:54:37 +0300

    Opcodes

--- opcodes.c
+++ opcodes.c
@@ -21,6 +21,7 @@ void pop(stack_t **stack, unsigned int line_number)
 	if (!(*stack))
 	{
 		fprintf(stderr, "L<%d>: can't pop an empty stack\n", line_number);
+		free(line);
 		exit(EXIT_FAILURE);
 	}
 	(*stack) = (*stack)->next;
